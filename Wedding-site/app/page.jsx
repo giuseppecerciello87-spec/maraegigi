cad43b65294cdff18863f1e7cdfdf2959b8a7e6d
giuseppecerciello87-spec <giuseppecerciello87@gmail.com>
2026-03-30 13:48:54 +0200

    Update page.jsx

--- Wedding-site/app/page.jsx
+++ Wedding-site/app/page.jsx
@@ -1,3 +1,4 @@
+"use client"; // <--- obbligatorio per Client Component
 import { Card, CardContent } from "../../components/ui/card";
 import { Button } from "../../components/ui/button";
 import { motion } from "framer-motion";
